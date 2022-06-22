3c9c5ded371735b68111f80b4130ead50b7a1a8a
Amanulla <1add4all@gmail.com>
2022-06-22 17:54:34 +0530

    Fixed some errors

--- app.js
+++ app.js
@@ -3,20 +3,21 @@ const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.openMenu');
 const menu_items = document.querySelectorAll('nav .mainMenu li a');
 
-openMenu.addEventListener('click',show);
-closeMenu.addEventListener('click',close);
+function show() {
+  mainMenu.style.display = 'flex';
+  mainMenu.style.top = '0';
+}
 
-// close menu when you click on a menu item 
+function close() {
+  mainMenu.style.top = '-100%';
+}
+
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', close);
+
+// close menu when you click on a menu item
 menu_items.forEach(item => {
-    item.addEventListener('click',function(){
-        close();
-    })
-})
-
-function show(){
-    mainMenu.style.display = 'flex';
-    mainMenu.style.top = '0';
-}
-function close(){
-    mainMenu.style.top = '-100%';
-}
+  item.addEventListener('click', function() {
+    close();
+  });
+});
