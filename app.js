a78585ab54358303aff4061a3679f1dc474f56c6
Amanulla <1add4all@gmail.com>
2022-06-22 18:22:27 +0530

    Fixed errors

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const mainMenu = document.querySelector('.mainMenu');
 const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.openMenu');
-const menu_items = document.querySelectorAll('nav .mainMenu li a');
+const menuItems = document.querySelectorAll('nav .mainMenu li a');
 
 function show() {
   mainMenu.style.display = 'flex';
@@ -15,9 +15,8 @@ function close() {
 openMenu.addEventListener('click', show);
 closeMenu.addEventListener('click', close);
 
-// close menu when you click on a menu item
-menu_items.forEach(item => {
-  item.addEventListener('click', function() {
+menuItems.forEach((item) => {
+  item.addEventListener('click', () => {
     close();
   });
 });
