b02356041f2e1641ac00eb2a105ad75630098893
Amanulla <1add4all@gmail.com>
2022-06-24 15:20:18 +0530

    Again newly added all the required files to the project as per the reviewer suggesion

--- app1.js
+++ app1.js
@@ -1,44 +1,35 @@
 // Get the modal
 
-var modal = document.getElementsByClassName("myModal");
-var btn = document.getElementsByClassName("myBtn");
-var span = document.getElementsByClassName("close");
+const modal = document.getElementsByClassName('myModal');
+const btn = document.getElementsByClassName('myBtn');
+const span = document.getElementsByClassName('close');
 
 function setDataIndex() {
-
-    for (i = 0; i < btn.length; i++)
-    {
-        btn[i].setAttribute('data-index', i);
-        modal[i].setAttribute('data-index', i);
-        span[i].setAttribute('data-index', i);
-    }
-    
+  for (let i = 0; i < btn.length; i + 1) {
+    btn[i].setAttribute('data-index', i);
+    modal[i].setAttribute('data-index', i);
+    span[i].setAttribute('data-index', i);
+  }
 }
 
-window.onload = function() {
-
-    setDataIndex();
+window.onload = function () {
+  setDataIndex();
 };
 
-for (i = 0; i < btn.length; i++)
-{
-    btn[i].onclick = function() {
-        var ElementIndex = this.getAttribute('data-index',i);
-        modal[ElementIndex].style.display = "block";
-    };
+for (let i = 0; i < btn.length; i + 1) {
+  btn[i].onclick = function () {
+    const ElementIndex = this.getAttribute('data-index', i);
+    modal[ElementIndex].style.display = 'block';
+  };
 
-    span[i].onclick = function() {
-        var ElementIndex = this.getAttribute('data-index',i);
-        modal[ElementIndex].style.display = "none";
-    };
-
+  span[i].onclick = function () {
+    const ElementIndex = this.getAttribute('data-index', i);
+    modal[ElementIndex].style.display = 'none';
+  };
 }
 
-
-window.onclick = function(event) {
-    if (event.target === modal[event.target.getAttribute('data-index')]) {
-        modal[event.target.getAttribute('data-index')].style.display = "none";
-    }
+window.onclick = function (event) {
+  if (event.target === modal[event.target.getAttribute('data-index')]) {
+    modal[event.target.getAttribute('data-index')].style.display = 'none';
+  }
 };
-
-
